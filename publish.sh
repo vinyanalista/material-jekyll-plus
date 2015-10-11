8e966bd9b9fe6d79cc0fbc98befa7ad961bd704a
Antônio Vinícius <vinyanalista@gmail.com>
2015-10-10 22:27:54 -0300

    Publishing script

---
+++ publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SOURCE="vinyanalista-website"
+TARGET="vinyanalista.github.io"
+
+git add --all .
+git stash
+LAST_COMMIT_HASH=`git rev-parse HEAD`
+jekyll build
+cd "../$TARGET"
+git pull
+git rm -rf *
+touch .nojekyll
+cp -r "../$SOURCE/_site/*" .
+git add --all .
+git commit -m "Publishing website (see commit $LAST_COMMIT_HASH from $SOURCE)"
+git push
+cd "../$SOURCE"
+git stash pop
