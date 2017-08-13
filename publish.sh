e99e63770ac58704e9fb4e0acca8f55fb197d27b
Antônio Vinícius Menezes Medeiros <vinyanalista@gmail.com>
2017-08-13 12:55:09 -0300

    livesuse-website contributing back

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-SOURCE="vinyanalista-website"
-TARGET="vinyanalista.github.io"
+USER="livesuse"
+SOURCE="livesuse-website"
+TARGET="livesuse.github.io"
 
 git add --all .
 git stash
@@ -12,7 +13,7 @@ git rm -rf *
 touch .nojekyll
 cp -r ../$SOURCE/_site/* .
 git add --all .
-git commit -m "Publishing website (see commit $LAST_COMMIT_HASH from $SOURCE)"
+git commit -m "Publishing website (see commit $USER/$SOURCE@$LAST_COMMIT_HASH)"
 git push
 cd "../$SOURCE"
 git stash pop
